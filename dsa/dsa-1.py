a4917818b47496c8aad3110d55bb9c9ccad8f6b9
deepaksinghea <deepak.singh.ea@gmail.com>
2021-06-04 22:22:51 +0530

    practice

---
+++ dsa/dsa-1.py
@@ -0,0 +1,13 @@
+mylist = ['deepak','ankit','yash']
+
+print(type(mylist))
+
+print(len(mylist))
+
+second_list = [1, 3.0, True, 'example']
+
+print(second_list)
+
+for item in mylist:
+    print(mylist.index(item), item)
+
